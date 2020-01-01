262e3755b0458dcd638be2d8ff3048b805dd880e
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2020-01-01 19:26:10 +0100

    updated frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,21 +8,37 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { BrowserRouter } from "react-router-dom";
+import { setContext } from "apollo-link-context";
+
+import { AUTH_TOKEN } from "./constants";
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN);
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ""
+        }
+    };
+});
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000"
+    uri: "http://localhost:4000"
 });
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
