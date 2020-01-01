eab9b15138ee8131351e42a19227211b83e1945f
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2020-01-01 21:56:29 +0100

    updated frontend

--- frontend/src/components/LinkList.js
+++ frontend/src/components/LinkList.js
@@ -3,47 +3,151 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
 import Link from "./Link";
+import { LINKS_PER_PAGE } from "../constants";
 
-const FEED_QUERY = gql`
-    {
-        feed {
+export const FEED_QUERY = gql`
+    query FeedQuery($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+        feed(first: $first, skip: $skip, orderBy: $orderBy) {
             links {
                 id
                 createdAt
                 url
                 description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+            count
+        }
+    }
+`;
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+    subscription {
+        newVote {
+            id
+            link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+            user {
+                id
             }
         }
     }
 `;
 
 class LinkList extends Component {
-    render() {
-        // const linksToRender = [
-        //     {
-        //         id: "1",
-        //         description: "Prisma turns your database into a GraphQL API 😎",
-        //         url: "https://www.prismagraphql.com"
-        //     },
-        //     {
-        //         id: "2",
-        //         description: "The best GraphQL client",
-        //         url: "https://www.apollographql.com/docs/react/"
-        //     }
-        // ];
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        const data = store.readQuery({ query: FEED_QUERY });
 
+        const votedLink = data.feed.links.find(link => link.id === linkId);
+        votedLink.votes = createVote.link.votes;
+
+        store.writeQuery({ query: FEED_QUERY, data });
+    };
+
+    _getQueryVariables = () => {
+        const isNewPage = this.props.location.pathname.includes("new");
+        const page = parseInt(this.props.match.params.page, 10);
+
+        const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+        const first = isNewPage ? LINKS_PER_PAGE : 100;
+        const orderBy = isNewPage ? "createdAt_DESC" : null;
+        return { first, skip, orderBy };
+    };
+
+    _subscribeToNewLinks = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev;
+                const newLink = subscriptionData.data.newLink;
+                const exists = prev.feed.links.find(
+                    ({ id }) => id === newLink.id
+                );
+                if (exists) return prev;
+
+                return Object.assign({}, prev, {
+                    feed: {
+                        links: [newLink, ...prev.feed.links],
+                        count: prev.feed.links.length + 1,
+                        __typename: prev.feed.__typename
+                    }
+                });
+            }
+        });
+    };
+
+    _subscribeToNewVotes = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_VOTES_SUBSCRIPTION
+        });
+    };
+
+    render() {
         return (
-            <Query query={FEED_QUERY}>
-                {({ loading, error, data }) => {
+            <Query query={FEED_QUERY} variables={this._getQueryVariables()}>
+                {({ loading, error, data, subscribeToMore }) => {
                     if (loading) return <div>Fetching</div>;
                     if (error) return <div>Error</div>;
+
+                    this._subscribeToNewLinks(subscribeToMore);
+                    this._subscribeToNewVotes(subscribeToMore);
 
                     const linksToRender = data.feed.links;
 
                     return (
                         <div>
-                            {linksToRender.map(link => (
-                                <Link key={link.id} link={link} />
+                            {linksToRender.map((link, index) => (
+                                <Link
+                                    key={link.id}
+                                    link={link}
+                                    index={index}
+                                    updateStoreAfterVote={
+                                        this._updateCacheAfterVote
+                                    }
+                                />
                             ))}
                         </div>
                     );
